e9d8c5c3d1383e4bd9eab9e4d57d8125b030c5ee
MarcelloS <marcello.sgr@gmail.com>
2022-09-07 21:58:29 +0200

    Reorganized code in order to handle time using TimeHandler class

--- main.py
+++ main.py
@@ -1,51 +1,5 @@
 import machine
 import utime
-
-###############################################################################
-#                               GLOBAL VARIABLES
-###############################################################################
-
-# Reference utime.localtime() returns (2022, 9, 4, 19, 40, 5, 6, 247) https://docs.micropython.org/en/v1.15/library/utime.html
-
-multiplex_selector = [(0,0,0,0),
-                      (1,0,0,0),
-                      (0,1,0,0),
-                      (1,1,0,0),
-                      (0,0,1,0),
-                      (1,0,1,0),
-                      (0,1,1,0),
-                      (1,1,1,0),
-                      (0,0,0,1),
-                      (1,0,0,1),
-                      (0,1,0,1),
-                      (1,1,0,1),
-                      (0,0,1,1),
-                      (1,0,1,1),
-                      (0,1,1,1),
-                      (1,1,1,1)]
-
-# the multiplexer will be controlled by s0, s1, s2, s3 and the prefix "a_" will refer to the analog
-#   and "d_" will refer to the digital
-
-a_s0 = machine.Pin(21, machine.Pin.OUT)
-a_s1 = machine.Pin(20, machine.Pin.OUT)
-a_s2 = machine.Pin(19, machine.Pin.OUT)
-a_s3 = machine.Pin(18, machine.Pin.OUT)
-a_sig = machine.ADC(machine.Pin(26))
-
-d_s0 = machine.Pin(10, machine.Pin.OUT)
-d_s1 = machine.Pin(11, machine.Pin.OUT)
-d_s2 = machine.Pin(12, machine.Pin.OUT)
-d_s3 = machine.Pin(13, machine.Pin.OUT)
-d_sig = machine.Pin(15, machine.Pin.OUT)
-
-IRRIGATION_TIMEOUT = 5 # seconds
-
-MAXIMUM_DIGITAL_CHANNELS = 7
-
-START_TIME = set()
-CURRENT_TIME = set()
-
 ###############################################################################
 #                               CLASSES
 ###############################################################################
@@ -150,6 +104,51 @@ class TimeHandler:
             raise ValueError("Seconds have to be a positive number")
 
 ###############################################################################
+#                               GLOBAL VARIABLES
+###############################################################################
+
+# Reference utime.localtime() returns (2022, 9, 4, 19, 40, 5, 6, 247) https://docs.micropython.org/en/v1.15/library/utime.html
+
+multiplex_selector = [(0,0,0,0),
+                      (1,0,0,0),
+                      (0,1,0,0),
+                      (1,1,0,0),
+                      (0,0,1,0),
+                      (1,0,1,0),
+                      (0,1,1,0),
+                      (1,1,1,0),
+                      (0,0,0,1),
+                      (1,0,0,1),
+                      (0,1,0,1),
+                      (1,1,0,1),
+                      (0,0,1,1),
+                      (1,0,1,1),
+                      (0,1,1,1),
+                      (1,1,1,1)]
+
+# the multiplexer will be controlled by s0, s1, s2, s3 and the prefix "a_" will refer to the analog
+#   and "d_" will refer to the digital
+
+a_s0 = machine.Pin(21, machine.Pin.OUT)
+a_s1 = machine.Pin(20, machine.Pin.OUT)
+a_s2 = machine.Pin(19, machine.Pin.OUT)
+a_s3 = machine.Pin(18, machine.Pin.OUT)
+a_sig = machine.ADC(machine.Pin(26))
+
+d_s0 = machine.Pin(10, machine.Pin.OUT)
+d_s1 = machine.Pin(11, machine.Pin.OUT)
+d_s2 = machine.Pin(12, machine.Pin.OUT)
+d_s3 = machine.Pin(13, machine.Pin.OUT)
+d_sig = machine.Pin(15, machine.Pin.OUT)
+
+IRRIGATION_TIMEOUT = 5 # seconds
+
+MAXIMUM_DIGITAL_CHANNELS = 7
+
+START_TIME = TimeHandler()
+CURRENT_TIME = TimeHandler()
+
+###############################################################################
 #                               FUNCTIONS
 ###############################################################################
 
@@ -170,9 +169,12 @@ def init_mux_analog():
         #a_sig.value(False) #ADC objects has no attribute 'value'
     
 def init_global_variables():
-    global START_TIME
-    START_TIME = utime.localtime()
-    CURRENT_TIME = utime.localtime()
+    global START_TIME, CURRENT_TIME
+    START_TIME = TimeHandler()
+    START_TIME.initialize(utime.localtime())
+
+    CURRENT_TIME = TimeHandler()
+    CURRENT_TIME.initialize(utime.localtime())
 
 def init():
     init_mux_digital()
@@ -213,7 +215,7 @@ init()
 sem = 0
 
 while True:
-    CURRENT_TIME = utime.localtime()
+    CURRENT_TIME.initialize(utime.localtime())
 
     for channel in range(0, MAXIMUM_DIGITAL_CHANNELS):
         print("Setting {}".format(channel))
