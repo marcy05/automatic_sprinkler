7610044c6da8682c27e050de53c8c2697cc7ed61
MarcelloS <marcello.sgr@gmail.com>
2023-03-25 22:39:51 +0100

    Added Pump class and HwInterface

--- SourceCode/srpinkler_2.py
+++ SourceCode/srpinkler_2.py
@@ -1,5 +1,6 @@
 import utime
 import network
+import machine
 from my_secret import secret
 from umqttsimple import MQTTClient
 
@@ -121,6 +122,81 @@ class BackEndInterface:
         logger.debug("Connected to MQTT.")
 
 
+class HwInterface:
+    def __init__(self) -> None:
+        self.mux_channels_used = 7
+
+        self.__pump_sig = 10
+        self.__pump_s3 = 11
+        self.__pump_s2 = 12
+        self.__pump_s1 = 13
+        self.__pump_s0 = 15
+
+        self.__sensor_sig = 26
+        self.__sensor_s3 = 18
+        self.__sensor_s2 = 19
+        self.__sensor_s1 = 20
+        self.__sensor_s0 = 21
+
+        self.d_s0 = machine.Pin(self.__pump_s0, machine.Pin.OUT)
+        self.d_s1 = machine.Pin(self.__pump_s1, machine.Pin.OUT)
+        self.d_s2 = machine.Pin(self.__pump_s2, machine.Pin.OUT)
+        self.d_s3 = machine.Pin(self.__pump_s3, machine.Pin.OUT)
+        self.d_sig = machine.Pin(self.__pump_sig, machine.Pin.OUT)
+
+        self.a_s0 = machine.Pin(self.__sensor_s0, machine.Pin.OUT)
+        self.a_s1 = machine.Pin(self.__sensor_s1, machine.Pin.OUT)
+        self.a_s2 = machine.Pin(self.__sensor_s2, machine.Pin.OUT)
+        self.a_s3 = machine.Pin(self.__sensor_s3, machine.Pin.OUT)
+        self.a_sig = machine.ADC(self.__sensor_sig)
+
+        self.multiplex_selector = [(0, 0, 0, 0),
+                                   (1, 0, 0, 0),
+                                   (0, 1, 0, 0),
+                                   (1, 1, 0, 0),
+                                   (0, 0, 1, 0),
+                                   (1, 0, 1, 0),
+                                   (0, 1, 1, 0),
+                                   (1, 1, 1, 0),
+                                   (0, 0, 0, 1),
+                                   (1, 0, 0, 1),
+                                   (0, 1, 0, 1),
+                                   (1, 1, 0, 1),
+                                   (0, 0, 1, 1),
+                                   (1, 0, 1, 1),
+                                   (0, 1, 1, 1),
+                                   (1, 1, 1, 1)]
+
+    def reset_digital_mux(self):
+        logger.debug("HwInterface - Resetting all relays...")
+        for channel in range(self.mux_channels_used):
+            self.d_s0.value(self.multiplex_selector[channel][0])
+            self.d_s1.value(self.multiplex_selector[channel][1])
+            self.d_s2.value(self.multiplex_selector[channel][2])
+            self.d_s3.value(self.multiplex_selector[channel][3])
+            self.d_sig.value(False)
+        logger.debug("HwInterface - Reset done.")
+
+    def set_channel_value(self, channel: int, signal: bool):
+        logger.debug("HwInterface - Channel: {} with Value: {}".format(channel,
+                                                                       signal))
+        self.d_s0.value(self.multiplex_selector[channel][0])
+        self.d_s1.value(self.multiplex_selector[channel][1])
+        self.d_s2.value(self.multiplex_selector[channel][2])
+        self.d_s3.value(self.multiplex_selector[channel][3])
+        self.d_sig.value(signal)
+
+class Pump:
+    def __init__(self, pump_id: int) -> None:
+        self.pump_id = pump_id
+        self.status = False
+
+    def set_pump_value(self, signal: bool):
+        logger.debug("Pump: {}, setting value: {}".format(self.pump_id,
+                                                          signal))
+        hw_interface = HwInterface()
+        hw_interface.set_channel_value(self.pump_id, signal)
+
 
 class Garden:
     def __init__(self) -> None:
@@ -131,6 +207,8 @@ class Garden:
 
         self.exec_update_interval = 30  # Time in seconds
         self.log_update_interval = 20  # Time in seconds
+
+        self.pumps = [Pump(i) for i in range(7)]
 
         logger.debug("Init completed.")
 
@@ -155,6 +233,10 @@ class Garden:
         return False
 
     def run(self):
+
+        if self._is_running_update_time_expired():
+            self.pumps[0].set_pump_value(True)
+
         if self._is_logging_update_time_expired():
             logger.debug("Running garden")
 
@@ -166,7 +248,7 @@ logger = SimpleLogger()
 my_garden = Garden()
 my_garden.init_backend()
 
-
+HwInterface().reset_digital_mux()
 
 # #############################################################################
 #                               MAIN LOOP
