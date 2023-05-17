44a2ff1a1914e48dd207329838e981375fa5026d
Marcello Sgarbi <marcello.sgr@gmail.com>
2023-05-17 22:09:04 +0200

    Added backend wifi connection

--- SourceCode/controller/controller.py
+++ SourceCode/controller/controller.py
@@ -1,6 +1,37 @@
 import machine
 import utime
+import network
 from umqtt.simple import MQTTClient
+from secret import secret
+
+class MqttClient:
+    def __init__(self):
+        self.wlan = network.WLAN(network.STA_IF)
+        self.network_ssid = secret["network_ssid"]
+        self.network_password = secret["network_password"]
+        self.mqtt_server_ip = secret["mqtt_server_ip"]
+        self.client_id = secret["client_id"]
+        self.mqtt_user = secret["mqtt_user"]
+        self.mqtt_password = secret["mqtt_password"]
+
+        self.connect()
+    
+    def connect(self):
+        print("Connecting...")
+        self.wlan.active(True)
+        self.wlan.connect(self.network_ssid, self.network_password)
+        max_retries = 20
+        for i in range(max_retries):
+            if not self.wlan.isconnected():
+                print("Connection retry {}/{}".format(i+1, max_retries))
+                utime.sleep(1)
+            else:
+                print("Connected.")
+                self.network_status = True
+                break
+        if not self.wlan.isconnected():
+            print("Not possible to connect to internet.")
+
 class Pump:
     def __init__(self, pump_id:int = 99, button_gpio:int = 99, red_gpio:int = 99, green_gpio:int = 99):
         self.pump_id: int = pump_id
@@ -23,6 +54,7 @@ p5 = Pump(5, 10, 12, 13)
 p6 = Pump(6, 11, 14, 15)
 pump_list = [p0, p1, p2, p3, p4, p5, p6]
 
+# ####  GLOBAL FUNCTIONS
 
 def set_on_red():
     for pump in pump_list:
@@ -53,15 +85,16 @@ def start_animation():
         pump.green.value(1)
         utime.sleep(.1)
         pump.green.value(0)
-
     print("Finish animation")
 
 # ####  MAIN
+
+backend = MqttClient()
 set_off_green()
 set_off_red()
 start_animation()
 
-
+print("Entering infinite loop")
 while True:
 
     if not p0.button.value():
