b87e568986b877e7f06fc6e2228d42cb687d94c2
marcy05 <37953351+marcy05@users.noreply.github.com>
2023-07-29 17:43:59 +0200

    removed

--- SourceCode/my_secret.py
+++
@@ -1,6 +0,0 @@
-secret = {"network_ssid": "netword_ssid",
-          "network_password": "network_password",
-          "mqtt_server_ip": "mqtt_server_ip_if_present",
-          "client_id": "mqtt_client_id_if_present",
-          "mqtt_user": "mqtt_user_if_present",
-          "mqtt_password": "mqtt_password_if_present"}
