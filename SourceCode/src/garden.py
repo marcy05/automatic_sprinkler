186716678f492653ed29663867c650a74650202f
Marcello Sgarbi <marcello.sgr@gmail.com>
2024-08-17 08:41:35 +0200

    Commented out dev watering period

--- SourceCode/src/garden.py
+++ SourceCode/src/garden.py
@@ -29,7 +29,7 @@ class Garden:
         self.watering_timer = utime.time()
         self.daily_watering_done = False
         self.watering_period = 1 * 24 * 60 * 60  # Days in seconds
-        self.watering_period = 50  # TODO erase this for real application
+        # self.watering_period = 50  # TODO erase this for real application
         self.watering_iterations = get_int_from_json("garden_watering_iteration", self._timers_persistency_path)
         self.watering_itersations_delay = get_int_from_json("garden_water_iteration_delay", self._timers_persistency_path)  # seconds of delays between one watering action and another.
 
